12d26ba762254ffcc07777ef0328d0337ad7a1b1
Alex Genzor <alexandros.genzor@fundacionmontessori.com>
2025-08-26 12:46:30 +0200

    Created Scripts folder. - Imported LabelOverride script from .unitypackage (just in case). - Created LightController script and added as component to LightManager.

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public GameObject lights;
+    public Light[] lightList;
+    public bool effectsToggle;
+    public int effectSelector;
+    public float deltaMult;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    
+}
