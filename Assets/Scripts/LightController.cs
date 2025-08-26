982d6e6e998dd8f5444509288b7d28a38c329a1d
Alex Genzor <alexandros.genzor@fundacionmontessori.com>
2025-08-26 13:33:05 +0200

    Added lights toggle with Q, and effects toggle with W (effect defaults to effect No.1 (strobe)). Made a small fix from explanation: ResetLights() also sets the light toggle bool to true.

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -1,27 +1,113 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
     public GameObject lights;
-    public Light[] lightList;
+    public List<Light> lightList;
+    public bool lightsToggle = true;
     public bool effectsToggle;
-    public int effectSelector;
-    public float deltaMult;
+    public int effectSelect;
+    public float speed;
+    public float delayTime;
     
+    /*
+     * Keys:
+     *  - Q: Toggles lights on / off.
+     *  - W: Controls lights coroutine.
+     *  - 1, 2, 3: Sets the lights behaviours.
+     */
+    
+    #region LIFECYCLE FUNCTIONS
     // Start is called before the first frame update
     void Start()
     {
-        
+        lightList.AddRange(lights.GetComponentsInChildren<Light>());
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+            ToggleLights();
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            effectsToggle = !effectsToggle;
+            
+            if (effectsToggle)
+                StartCoroutine(ICoroutine());
+
+            else
+            {
+                StopCoroutine(ICoroutine());
+                ResetLights();
+                
+            }
+            
+        }
+
+    }
+    
+    #endregion
+
+    private void ToggleLights()
+    {
+         lightsToggle = !lightsToggle;
+
+         foreach (var lightInst in lightList)
+         {
+             lightInst.intensity = (lightsToggle ? 2 : 0);
+
+         }
         
+    }
+
+    private void ResetLights()
+    {
+        lightsToggle = true;
         
+        foreach (var lightInst in lightList)
+        {
+            lightInst.intensity = 2;
+
+        }
+        
+    }
+
+    IEnumerator ICoroutine()
+    {
+        while (effectsToggle)
+        {
+            switch (effectSelect)
+            {
+                case 1:
+                    ToggleLights();
+                    yield return new WaitForSeconds(delayTime);
+
+                    break;
+                
+                case 2:
+                    
+
+                    break;
+                
+                case 3:
+                    
+
+                    break;
+                
+            }
+
+            yield return null;
+
+        }
+
+        yield return null;
+
     }
     
 }
